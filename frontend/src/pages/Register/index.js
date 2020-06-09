9e98ddc731f571ec8c9ac791bd04a6483e6e005c
Felipe Yui <felipe_m87@yahoo.com.br>
2020-06-09 16:48:22 +0930

    add API IBGE

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,23 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Link , useHistory} from 'react-router-dom';
+import axios from 'axios';
+
+import { Map, TileLayer, Marker} from 'react-leaflet'
+import { LeafletMouseEvent } from 'leaflet'
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import './style.css';
 import Logoimg from '../../assests/logo.svg'
 
 import api from '../../services/api'
 
-export default function Register(){
+const Register = () => {
+
+    const [selectedUF, setSelectedUF] = useState('0')
+    const [selectedCity, setSelectedCity] = useState('0')
+
+    const [selectedPosition, setSelectedPosition] = useState([0, 0])
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [whatsapp, setWhatsapp] = useState('');
-    const [city, setCity] = useState('');
-    const [uf, setUf] = useState('');
+    
     const history = useHistory();
 
-    async function handleRegister(e){
+    const [ufs, setUfs] = useState([])
+    const [cities, setCities] = useState([])
+
+    async function handleRegister(e) {
         e.preventDefault();
+
+        const uf = selectedUF
+        const city = selectedCity
 
         const data = {
             name,
@@ -26,70 +40,114 @@ export default function Register(){
             city,
             uf,
         };
-        try{
-        const response = await api.post('/ongs', data);
+        try {
+            const response = await api.post('/ongs', data);
 
-        alert(`Seu ID de acesso: ${response.data.id} `);
+            alert(`Seu ID de acesso: ${response.data.id} `);
 
-        history.push('/');
-        } catch (err){
+            history.push('/');
+        } catch (err) {
             alert('Erro no cadastro, tente novamente.');
         }
+
+
+    }
+
+    useEffect(() => {
+        axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            .then(response => {
+                const ufInitials = response.data.map(uf => uf.sigla).sort();
+
+                setUfs(ufInitials)
+            })
+    }, []);
+
+    useEffect(() => {
+        if (selectedUF === '0') {
+            return;
+        }
+        axios
+            .get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUF}/municipios`)
+            .then(response => {
+                const cityNames = response.data.map(city => city.nome).sort();
+
+                setCities(cityNames);
+            });
+    }, [selectedUF]);
+
+    function handleSelectUf(event) {
+        const uf = event.target.value
+        setSelectedUF(uf);
+    };
+
+    function handleSelectCity(event) {
+        const city = event.target.value
+
+        console.log(event.target)
+        setSelectedCity(city);
     }
     return (
         <div className="register-container">
-                <div className="content">
-                    <section>
-                        <img src={Logoimg} alt="Be The Hero"/>
+            <div className="content">
+                <section>
+                    <img src={Logoimg} alt="Be The Hero" />
 
-                        <h1>Cadastro</h1>
-                        <p>
-                            Faça seu cadastro, entre na plataforma e ajude pessoas a 
-                            encontrarem os casos da sua ONG.
+                    <h1>Cadastro</h1>
+                    <p>
+                        Faça seu cadastro, entre na plataforma e ajude pessoas a
+                        encontrarem os casos da sua ONG.
                         </p>
-                        <Link className = "back-link"to="/">
-                        <FiArrowLeft size ={16} color= "#e02041"/>
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#e02041" />
                         Não tenho cadastro
 
                         </Link>
-                    </section>
+                </section>
 
-                        <form onSubmit={handleRegister}>
+                <form onSubmit={handleRegister}>
 
-                            <input 
-                                placeholder="Nome da ONG"
-                                value={name}
-                                onChange = {e=>setName(e.target.value)}    
-                            />
-                            <input 
-                                type="email" placeholder="E-mail"
-                                value={email}
-                                onChange = {e=>setEmail(e.target.value)}
-                            />
-                            <input 
-                                placeholder="Whatsapp"
-                                value={whatsapp}
-                                onChange = {e=>setWhatsapp(e.target.value)}
-                            />
+                    <input
+                        placeholder="Nome da ONG"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <input
+                        type="email" placeholder="E-mail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input
+                        placeholder="Whatsapp"
+                        value={whatsapp}
+                        onChange={e => setWhatsapp(e.target.value)}
+                    />
 
-                            <div className="input-group">
-                                <input 
-                                    placeholder="Cidade"
-                                    value={city}
-                                    onChange = {e=>setCity(e.target.value)}
-                                />
-                                <input 
-                                    placeholder="UF" style = {{ width:80}}
-                                    value={uf}
-                                    onChange = {e=>setUf(e.target.value)}
-                                />
-                            </div>
-                            <button className="button" type="submit">Cadastrar</button>
+                    <div className="select-group">
+                        
+                            <select name="uf" id="uf" value={selectedUF} onChange={handleSelectUf}>
+                                <option value="0">Selecione uma UF</option>
+                                {ufs.map(uf => (
+                                    <option key={uf} value={uf}>{uf}</option>
+                                ))}
+                            </select>
+                        
+                        
+                            <select name="city" id="city" value={selectedCity} onChange={handleSelectCity}>
+                                <option value="0">Selecione uma Cidade</option>
+                                {cities.map(city => (
+                                    <option key={city} value={city}>{city}</option>
+                                ))}
+                            </select>
+                        
+                    </div>
+                    <button className="button" type="submit">Cadastrar</button>
 
-                        </form>
-                    
-                </div>
+                </form>
+
+            </div>
 
         </div>
     );
 }
+
+export default Register;
